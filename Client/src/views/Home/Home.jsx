5d0fd8e7b161a17cf5ac6effc86a013bdd70f7ab
LiliSLG <silvialilianagarcia@hotmail.com>
2023-09-19 17:56:14 -0300

    Separamos repositorios de front y back

--- Client/src/views/Home/Home.jsx
+++
@@ -1,9 +0,0 @@
-import Cards from "../../components/Cards/Cards";
-
-const Home = ({characters, onClose}) => (
-    /* le pasamos por props la función onClose a Cards */
-    <Cards characters={characters} onClose={onClose} />
-  );
-  
-  export default Home;
-  
